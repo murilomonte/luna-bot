f1cb48a36fbcf83c6cfd98621e63da0b3fdd0b53
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2023-04-23 11:09:21 -0300

    Comando gsearch corrigido

--- command/other/gsearch.js
+++ command/other/gsearch.js
@@ -1,23 +1,26 @@
 const gsearch = require("googlethis");
+const lang = require("../../utils/text.json");
 
 exports.gsearch = async(luna) => {
-	if (!args.length > 0) return await luna.reply("'query' need for this command");
+	if (!args.length > 0) return await luna.reply("É preciso de um termo para pesquisa.");
 	let gResult,
 		data = null,
 		img = null,
-		// query = args.join(" ").replace(/\-(?:search|reverse|image)/g, "")
 		query = args.join(" ").replace(/\-(?:search|reverse|image)/g, "")
-
 	try {
+		await luna.reply("Irei provicenciar!");
 		(gResult = await gsearch.image(query, { safe: false })),
 			(data = gResult?.[Math.floor(Math.random() * gResult.length)]),
 			(img = data?.url ? data.url : "https://telegra.ph/file/177a7901780b35d7123c7.png");
+		
+		await luna.reply(lang.ptbr.util.download.progress);
 		await luna.replyWithPhoto(
 			{ url: img },
-			{ caption: `*Google Image Search*\n\n*${data?.origin?.title}*\nwidth: ${data?.width}\nheight: ${data?.height}` },
-		)			
+			{ caption: `**Pesquisa por imagem**\n\n*${data?.origin?.title}*\nLargura: ${data?.width}\nAltura: ${data?.height}` },
+		)		
+		await luna.reply("Prontinho!");
 	} catch (e) {
 		console.log(e);
-		await luna.reply(`Err:\n${e.message}`);
+		await luna.reply(`Erro:\n${e.message}`);
 	}
 }
